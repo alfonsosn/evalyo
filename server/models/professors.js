dc0b1c5b562a1b110ace0214b3344d7e148666af
lizraeli <leo2002b@yahoo.com>
2017-02-22 18:27:25 -0500

    fix schema

--- server/models/professors.js
+++ server/models/professors.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var ProfessorSchema = new Schema({
  firstname: {type: String, required: true},
  lastName:  {type: String, required: true},
- questions: [Schema.Types.Mixed]
+ courses: [Schema.Types.Mixed]
 });
 
 
