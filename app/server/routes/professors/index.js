d8403620fc9b52d3be20743156b709d4a0572dd5
lizraeli <leo2002b@yahoo.com>
2017-03-21 01:36:49 -0400

    added course page

--- app/server/routes/professors/index.js
+++ app/server/routes/professors/index.js
@@ -3,6 +3,10 @@ var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require("fs"))
 var _ = require('lodash'); //extend
 
+const organization_q_ids = [3, 15, 16, 19, 20, 17]
+const experience_q_ids = [11, 12, 14, 8]
+const clarity_q_ids = [1, 4, 5, 6, 13]
+const personality_q_ids = [2, 7, 10]
 
 // helper functions
 var getProf = function(file) {
@@ -19,14 +23,30 @@ var parseProfJSON = function(data){
     return prof
   }
 
-var obtainReviews = function(professors, courseId){
-  var reviews = []
-  professor.courses.forEach(function(element){
-    if (element.subject == courseId)
-      reviews.push(element);
-  });
-  return reviews
+const getRatings = (professor, courseId) => {
+  const course = professor.courses.find((course) => course.subject == courseId)
+  const ratings = course.questions.map((question) => 
+    question.id <= 9 ?
+      Object.assign(question, {
+        average: Math.round(question.average / 7 * 100)
+      })
+    : question.id <= 14 ? 
+      Object.assign(question, {
+        average: Math.round(question.average / 3 * 100)
+      })
+    : question)
+
+  return ratings
 }
+
+const getCourseInfo = (professor, courseId) => {
+  const course = professor.courses.find((course) => course.subject == courseId)
+  return {
+    semester: course.semester,
+    subject: course.subject
+  }
+}
+
 
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
@@ -59,10 +79,41 @@ router.get('/:prof/:course', function(req, res, next) {
 
   getProf(file).then(function(data) {
     professor = parseProfJSON(data)
-    var reviews =  obtainReviews(professor, courseId)
-    res.json(reviews);
-  });
-});
+    const courseInfo = getCourseInfo(professor, courseId)
+    const questions =  getRatings(professor, courseId)
+
+    // Split questions into categories
+    const organization_questions = questions.filter((question) => 
+      organization_q_ids.includes(question.id))
+
+    const clarity_questions = questions.filter((question) => 
+      clarity_q_ids.includes(question.id))
+
+    const personality_questions = questions.filter((question) => 
+      personality_q_ids.includes(question.id))
+  
+    let experience_questions = questions.filter((question) => 
+      experience_q_ids.includes(question.id))
+    // get # of times professor taught this course
+    const timesTaught = professor.courses.reduce((total, course) => 
+      course.subject == courseId ? total + 1 : total , 0)
+
+    experience_questions.push({
+      id: 'XX',
+      question: 'Times the professor taught this class',
+      average: timesTaught
+    })  
+
+    res.render('course', {
+      professor: professor,
+      course: courseInfo, 
+      clarity: clarity_questions,
+      organization: organization_questions,
+      experience: experience_questions,
+      personality: personality_questions
+    })
+  })
+})
 
 
 
