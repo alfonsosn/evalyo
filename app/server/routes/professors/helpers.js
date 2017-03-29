241f11037e36360efb899a25e1279e64630314ae
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-03-29 16:02:53 -0400

    created an aggregator function for ratings

--- app/server/routes/professors/helpers.js
+++ app/server/routes/professors/helpers.js
@@ -6,6 +6,8 @@ const experience_q_ids = [11, 12, 14, 8];
 const clarity_q_ids = [1, 4, 5, 6, 13];
 const personality_q_ids = [2, 7, 10];
 
+// HELPER functions
+
 const roundAverage = (element, num) => {
   element.average = Math.round(element.average / num * 100)
   return element
@@ -23,43 +25,84 @@ const getSingleRatings = (semester) => {
   return ratings
 }
 
-// // Ratings Class
-class Ratings {
-setQuestions(ids) {
-  return this.ratings.filter((question) =>
-  ids.includes(question.id));
-}
+// needs refactoring
 
-constructor(semester, times_taught) {
-  this.ratings = getSingleRatings(semester);
-  this.semester = semester.semester;
-  this.organization = this.setQuestions(organization_q_ids);
-  this.clarity = this.setQuestions(clarity_q_ids);
-  this.personality = this.setQuestions(personality_q_ids);
-  this.experience = this.setQuestions(experience_q_ids);
-  this.times_taught = times_taught;
-  this.experience.push({
-    id: 'XX',
-    question: 'Times the professor taught this class',
-    average: times_taught
+const aggregateRatings = (ratings_array) => {
+  ratings_array.forEach((element, index, arr) => {
+    element.count = 1
+    for (var j = index + 1; j < arr.length; j++){
+      if (element.id === arr[j].id) {
+        if (_.has(element, 'average')){
+          element.average += arr[j].average;
+        }
+        else if (_.has(element, 'average')){
+          element.yes = filterInt(element.yes);
+          element.no = filterInt(element.no);
+          element.noAnswer = filterInt(element.noAnswers);
+          element.yes += filterInt(arr[j].yes);
+          element.no += filterInt(arr[j].no);
+          element.noAnswer += filterInt(arr[j].noAnswers);
+        }
+        element.count++;
+        arr.splice(j, 1);
+      }
+    }
+    if (element.id <= 15){
+      element.average = element.average / element.count
+      element.average = _.round(element.average, 2);
+    }
   });
 }
-}
 
-class Evaluation {
-setEvaluation (semesters) {
-  semesters.forEach((rating) => {
-      this.evaluations.push(new Ratings(rating, this.times_taught))
-    });
+// Ratings Class
+class Ratings {
+  setQuestions(ids) {
+    return this.ratings.filter((question) =>
+    ids.includes(question.id));
   }
 
-constructor(semester_ratings, times_taught){
-  this.evaluations = []
-  this.times_taught = times_taught
-  this.setEvaluation(semester_ratings)
-}
+  constructor(semester, times_taught) {
+    this.ratings = getSingleRatings(semester);
+    this.semester = semester.semester;
+    this.organization = this.setQuestions(organization_q_ids);
+    this.clarity = this.setQuestions(clarity_q_ids);
+    this.personality = this.setQuestions(personality_q_ids);
+    this.experience = this.setQuestions(experience_q_ids);
+    this.times_taught = times_taught;
+    this.experience.push({
+      id: 'XX',
+      question: 'Times the professor taught this class',
+      average: times_taught
+    });
+  }
 }
 
+// Evaluations class
+
+class Evaluation {
+  setEvaluation (semesters) {
+      let aggregate = [ ]
+      semesters.forEach((rating) => {
+        this.evaluations.push(new Ratings(rating, this.times_taught))
+      });
+      semesters.forEach((rating) => {
+        aggregate.push(rating.questions)
+      })
+      let all_questions = _.flatten(aggregate)
+      aggregateRatings(all_questions)
+      let aggregateRating = {};
+      aggregateRating.semester = 'Aggregate';
+      aggregateRating.semester = 'Aggregate';
+      aggregateRating.questions = all_questions
+      console.log(aggregateRating)
+    }
+
+  constructor(semester_ratings, times_taught){
+    this.evaluations = []
+    this.times_taught = times_taught
+    this.setEvaluation(semester_ratings)
+  }
+}
 
 // EXPORT FUNCTIONS
 
@@ -67,21 +110,6 @@ exports.aggregatesExperience = (professors, courseId) => (
   professors.courses.reduce((total, course) =>
     course.subject === courseId ? total + 1 : total , 0)
 )
-
-exports.aggregateRatings = (ratings_array) => {
-  ratings_array.forEach((element, index, arr) => {
-    element.count = 1
-    for (var j = index + 1; j < arr.length; j++){
-      if (element.id === arr[j].id){
-        element.average += arr[j].average;
-        element.count++;
-        arr.splice(j, 1);
-      }
-    }
-    element.average = element.average / element.count
-  });
-}
-
 
 exports.getSemesters = (professorArray, id) => {
   return professorArray.courses.filter((course) => course.subject === id)
