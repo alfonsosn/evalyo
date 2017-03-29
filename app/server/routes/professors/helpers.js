1439806d887392615eb1c161be8f3a6ae5469a45
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-03-29 13:46:31 -0400

    created helper functions for routes

--- app/server/routes/professors/helpers.js
+++ app/server/routes/professors/helpers.js
@@ -1,116 +1,99 @@
-'use strict'
 var _ = require('lodash'); //extend
 
-module.exports = {
+// ARRAY variables for questions
+const organization_q_ids = [3, 15, 16, 19, 20, 17];
+const experience_q_ids = [11, 12, 14, 8];
+const clarity_q_ids = [1, 4, 5, 6, 13];
+const personality_q_ids = [2, 7, 10];
 
-  // array variables for questions
-  const organization_q_ids = [3, 15, 16, 19, 20, 17]
-  const experience_q_ids = [11, 12, 14, 8]
-  const clarity_q_ids = [1, 4, 5, 6, 13]
-  const personality_q_ids = [2, 7, 10]
+const roundAverage = (element, num) => {
+  element.average = Math.round(element.average / num * 100)
+  return element
+}
 
-  // Helper functions
-  const onlyUnique = (value, index, self) => {
-    return self.indexOf(value) === index;
-  }
+const getSingleRatings = (semester) => {
+  let ratings = semester.questions.map((question) => {
+    if (question.id <= 9)
+      return roundAverage(question, 7)
+    else if (question.id <= 14)
+      return roundAverage(question, 3)
+    else
+      return question
+    })
+  return ratings
+}
 
-  const roundAverage = (element, num) => {
-    element.average = Math.round(element.average / num * 100)
-    return element
-  }
+// // Ratings Class
+class Ratings {
+setQuestions(ids) {
+  return this.ratings.filter((question) =>
+  ids.includes(question.id));
+}
 
-  const aggregatesExperience = (professors, courseId) => (
-    professors.courses.reduce((total, course) =>
-      course.subject === courseId ? total + 1 : total , 0)
-  );
+constructor(semester, times_taught) {
+  this.ratings = getSingleRatings(semester);
+  this.semester = semester.semester;
+  this.organization = this.setQuestions(organization_q_ids);
+  this.clarity = this.setQuestions(clarity_q_ids);
+  this.personality = this.setQuestions(personality_q_ids);
+  this.experience = this.setQuestions(experience_q_ids);
+  this.times_taught = times_taught;
+  this.experience.push({
+    id: 'XX',
+    question: 'Times the professor taught this class',
+    average: times_taught
+  });
+}
+}
 
-  const aggregateRatings = (ratings_array) => {
-    ratings_array.forEach((element, index, arr) => {
-      element.count = 1
-      for (var j = index + 1; j < arr.length; j++){
-        if (element.id === arr[j].id){
-          element.average += arr[j].average;
-          element.count++;
-          arr.splice(j, 1);
-        }
-      }
-      element.average = element.average / element.count
+class Evaluation {
+setEvaluation (semesters) {
+  semesters.forEach((rating) => {
+      this.evaluations.push(new Ratings(rating, this.times_taught))
     });
   }
 
-  // Parser from Server
-  const parseProfJSON = (data) => {
-      let prof = JSON.parse(data)
-      prof.courseTitles = []
-      prof.courses.forEach(element => {
-              prof.courseTitles.push((element.subject).split(' ').join('_'));
-          })
-      prof.courseTitles = prof.courseTitles.filter(onlyUnique).sort();
-      return prof
-    }
+constructor(semester_ratings, times_taught){
+  this.evaluations = []
+  this.times_taught = times_taught
+  this.setEvaluation(semester_ratings)
+}
+}
 
-  // Getter Functions
-  const getProf = (file) => {
-    return fs.readFileAsync(file, 'utf8');
-  }
 
-  const getSemesters = (professorArray, id) => {
-    return professorArray.courses.filter((course) => course.subject === id)
-  }
+// EXPORT FUNCTIONS
 
-  const getSingleRatings = (semester) => {
-    const ratings = semester.questions.map((question) => {
-      if (question.id <= 9)
-        return roundAverage(question, 7)
-      else if (question.id <= 14)
-        return roundAverage(question, 3)
-      else
-        return question
-      })
-    return ratings
-  }
+exports.aggregatesExperience = (professors, courseId) => (
+  professors.courses.reduce((total, course) =>
+    course.subject === courseId ? total + 1 : total , 0)
+)
 
-  const getCourseSubject = (professor, courseId) => {
-    const semester = professor.courses.find((course) => course.subject === courseId)
-      return {
-        subject: semester.subject
+exports.aggregateRatings = (ratings_array) => {
+  ratings_array.forEach((element, index, arr) => {
+    element.count = 1
+    for (var j = index + 1; j < arr.length; j++){
+      if (element.id === arr[j].id){
+        element.average += arr[j].average;
+        element.count++;
+        arr.splice(j, 1);
       }
-  }
-
-  // Ratings Class
-  class Ratings {
-    setQuestions(ids) {
-      return this.ratings.filter((question) =>
-      ids.includes(question.id));
     }
+    element.average = element.average / element.count
+  });
+}
 
-    constructor(semester, times_taught) {
-      this.ratings = getSingleRatings(semester);
-      this.semester = semester.semester;
-      this.organization = this.setQuestions(organization_q_ids);
-      this.clarity = this.setQuestions(clarity_q_ids);
-      this.personality = this.setQuestions(personality_q_ids);
-      this.experience = this.setQuestions(experience_q_ids);
-      this.times_taught = times_taught;
-      this.experience.push({
-        id: 'XX',
-        question: 'Times the professor taught this class',
-        average: times_taught
-      });
+
+exports.getSemesters = (professorArray, id) => {
+  return professorArray.courses.filter((course) => course.subject === id)
+}
+
+exports.getCourseSubject = (professor, courseId) => {
+  let semester = professor.courses.find((course) => course.subject === courseId)
+    return {
+      subject: semester.subject
     }
-  }
+}
 
-  class Evaluation {
-    setEvaluation (semesters) {
-      semesters.forEach((rating) => {
-          this.evaluations.push(new Ratings(rating, this.times_taught))
-        });
-      }
-
-    constructor(semester_ratings, times_taught){
-      this.evaluations = []
-      this.times_taught = times_taught
-      this.setEvaluation(semester_ratings)
-    }
-  }
+exports.newEvaluation = (semesters, times) => {
+  return new Evaluation(semesters, times)
 }
---
+++ app/server/routes/professors/parsers.js
@@ -0,0 +1,23 @@
+'use strict'
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require("fs"))
+
+// HELPER FUNCTIONS
+const onlyUnique = (value, index, self) => {
+  return self.indexOf(value) === index;
+}
+
+// Parser from Server
+exports.parseProfJSON = (data) => {
+  let prof = JSON.parse(data)
+  prof.courseTitles = []
+  prof.courses.forEach(element => {
+          prof.courseTitles.push((element.subject).split(' ').join('_'));
+      })
+  prof.courseTitles = prof.courseTitles.filter(onlyUnique).sort();
+  return prof
+}
+
+exports.getProf = (file) => {
+  return fs.readFileAsync(file, 'utf8');
+}
