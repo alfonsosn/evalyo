cb953ff9a57c5e7dd864763aac0e7351f2d43ca4
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-04-18 16:08:07 -0400

    deleted nav bar

--- app/client/components/Nav.js
+++ app/client/components/Nav.js
@@ -1,17 +1,11 @@
 'use strict';
 
 import React from 'react';
-import AppBar from 'react-toolbox/lib/app_bar';
-import Navigation from 'react-toolbox/lib/navigation';
-import Link from 'react-toolbox/lib/Link';
 
 export default class Nav extends React.Component {
   render() {
     return (
       <div>
-        <AppBar title='Evalyo'>
-
-        </AppBar>
         <div className="app-content">{this.props.children}</div>
        </div>
     );
