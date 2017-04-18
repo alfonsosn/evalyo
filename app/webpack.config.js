bc1d2cc3bbb5aa1cfa5d8756fed241821c5d2498
lizraeli <leo2002b@yahoo.com>
2017-04-18 18:14:14 -0400

    webpack.config & package.json

---
+++ app/webpack.config.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: debug ? 'inline-sourcemap' : null,
+  entry: path.join(__dirname, 'client', 'app-client.js'),
+
+  devServer: {
+    inline: true,
+    port: 3333,
+    contentBase: "server/static/",
+    historyApiFallback: {
+      index: '/index.html'
+    }
+  },
+
+  output: {
+    path: path.join(__dirname, 'server', 'static', 'js'),
+    publicPath: "/js/",
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|babel_cache|server)/,
+        loader: ['babel-loader'],
+        options: {
+          presets: ['env'],
+          plugins: [require('babel-plugin-transform-class-properties')]
+        },
+        query: {
+          cacheDirectory: 'babel_cache',
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss?sourceMap&sourceComments',
+<<<<<<< HEAD
+        ]
+      }
+    ]
+  },
+  postcss: () => {
+    return [
+=======
+        ],
+      }
+    ]
+  },
+
+  postcss: () => {
+  return [
+>>>>>>> cb953ff9a57c5e7dd864763aac0e7351f2d43ca4
+      /* eslint-disable global-require */
+      require('postcss-cssnext'),
+      /* eslint-enable global-require */
+    ];
+  },
+  plugins: debug ? [] : [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    }),
+  ]
+};
