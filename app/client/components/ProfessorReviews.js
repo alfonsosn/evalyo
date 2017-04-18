76c1b8963c85d85cf14cac2ff505a96c2118563c
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-04-18 12:54:30 -0400

    class reviews route complete

--- app/client/components/ProfessorReviews.js
+++ app/client/components/ProfessorReviews.js
@@ -8,9 +8,9 @@ export default class ProfessorReviews extends React.Component {
   constructor(props){
       super(props);
       this.state = {
-        courses: [],
-        courseTitles: [],
-        prof: ''
+        prof: '',
+        course: '',
+        evaluations: []
       };
   }
 
@@ -20,30 +20,17 @@ export default class ProfessorReviews extends React.Component {
       type: 'GET'
     })
     .done(reviews => {
+      this.setState({'course': this.props.params.course, 'prof': this.props.params.prof, 'evaluations': reviews})
       console.log(reviews)
-      // this.setState({'courses': courses, 'courseTitles': courses.courseTitles, 'prof': this.props.params.prof})
     });
   }
 
   render() {
     return (
       <div>
-
-
+        <h1> {this.state.prof} | {this.state.course}</h1>
+        <p> Aggregate Reviews for this professor </p>
       </div>
     );
   }
 }
-
-// <h1> {this.state.courses.firstName} {this.state.courses.lastName} </h1>
-// <p> This professor has previously taught: </p>
-//
-// <ul>
-//   {this.state.courseTitles.map((title, index)=>
-//     <li key={index}>
-//       <Link to={`/professor/${this.state.prof}/${title}`}>
-//       {title}
-//       </Link>
-//     </li>
-//   )}
-// </ul>
