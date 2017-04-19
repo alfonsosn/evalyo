a38a89af947be150dad7fb682e1289055464f293
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-04-19 14:37:27 -0400

    trying to change context of rebass

--- app/client/components/Nav.js
+++ app/client/components/Nav.js
@@ -5,26 +5,30 @@ import rebass from 'rebass'
 import { Button } from 'rebass'
 
 export default class Nav extends React.Component {
+
   getChildContext () {
     return {
-      rebass: {
-        fontSizes: [ 64, 48, 24, 18, 16, 14, 12],
-        Button: {
-          backgroundColor:  'rgb(238, 85, 68)'
+      rebass:
+        {
+          Button:
+          {
+            backgroundColor:  'purple'
+          }
         }
       }
     }
-  }
 
   render() {
-    return (
-      <div>
-        <div className="app-content">{this.props.children}</div>
-       </div>
-    );
-  }
-}
+      console.log("rebass", rebass.Button)
+      return (
+        <div>
+          <Button>By Departments</Button>
+          <div className="app-content">{this.props.children}</div>
+         </div>
+      );
+    }
 
-Nav.childContextTypes = {
-  rebass: React.PropTypes.object
-}
+    childContextTypes: {
+      rebass: React.PropTypes.object
+    }
+  }
