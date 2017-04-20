f4c84984e60b82a81c5bb65d84a7d515433e433d
lizraeli <leo2002b@yahoo.com>
2017-04-20 00:10:19 -0400

    rebass initial professor + review

--- app/client/components/Review.js
+++ app/client/components/Review.js
@@ -6,9 +6,24 @@ import $ from "jquery";
 
 export default class Review extends React.Component {
   render() {
+    const reviews = this.props.reviews
+     
     return (
       <div>
-        
+        {Object.keys(reviews).map((key) =>        
+          reviews[key] instanceof Array ?
+             reviews[key].map((topic) => (
+                <ul style={{listStyleType: 'none'}}>
+                  { Object.keys(topic).map((key) => 
+                      key === 'id' ? null
+                      : <li> <b>{key}</b>: {topic[key]} </li>
+                    )
+                  }
+                </ul>
+              )
+            )
+          : null      
+        )}
       </div>
     );
   }
