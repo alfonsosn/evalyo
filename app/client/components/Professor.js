880d602f33434c91cfd620c957398d99f5345f68
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-04-21 13:05:17 -0400

    reorganized function in professors page

--- app/client/components/Professor.js
+++ app/client/components/Professor.js
@@ -22,68 +22,6 @@ export default class Professor extends React.Component {
       this.handleSemesterChange = this.handleSemesterChange.bind(this)
   }
 
-  handleSemesterChange(e){
-    const semester = e.target.value
-    // if 'choose one' option was selected
-    if (semester === '') return
-    $.ajax({
-      url: `/api/professors/${this.state.prof}/${this.state.selectedCourse}/${semester}`,
-      type: 'GET'
-    })
-    .done(reviews => {
-      this.setState({
-        selectedSemester: semester,
-        reviews: reviews[0]
-      })
-    });
-  }
-
-  generateSemesterTitles(arr, subject){
-    const blankOption = [{ value: '', children: 'choose one'}]
-
-    const aggregateOption = [{
-      value: 'Aggregate',
-      label: 'Aggregate'
-    }]
-
-    const semesterTitles = arr.filter((course) =>
-        course.subject === subject)
-        .map((course) => {
-          return {
-              value: course.semester,
-              children: course.semester
-            }
-          }
-        )
-
-    return blankOption.concat(aggregateOption, semesterTitles)
-  }
-
-  handleCourseChange(e){
-    // const subject = course.replace(/_/g, " ")
-    const course = e.target.value
-
-    // if blank option was chosen
-    if (course === '') return
-
-    this.setState({
-      selectedCourse: course,
-      semesterTitles: this.generateSemesterTitles(this.state.courses, course)
-    });
- }
-
-  generateCourseTitles(titles){
-    const blankOption = [{ value: '', children: 'choose one'}]
-
-    const courseTitles = titles.map((title) => {
-      const title_with_spaces = title.replace(/_/g, " ")
-      return {
-        value: title_with_spaces,
-        children: title_with_spaces
-      }
-    })
-    return blankOption.concat(courseTitles)
-  }
 
   componentDidMount(){
     $.ajax({
@@ -100,6 +38,70 @@ export default class Professor extends React.Component {
     });
   }
 
+
+  generateCourseTitles(titles){
+    const blankOption = [{ value: '', children: 'choose one'}]
+
+    const courseTitles = titles.map((title) => {
+      const title_with_spaces = title.replace(/_/g, " ")
+      return {
+        value: title_with_spaces,
+        children: title_with_spaces
+      }
+    })
+    return blankOption.concat(courseTitles)
+  }
+
+  handleCourseChange(e){
+    // const subject = course.replace(/_/g, " ")
+    const courseTitle = e.target.value
+
+    // if blank option was chosen
+    if (courseTitle === '') return
+
+    this.setState({
+      selectedCourse: courseTitle,
+      semesterTitles: this.generateSemesterTitles(this.state.courses, courseTitle)
+    });
+ }
+
+ generateSemesterTitles(arr, subject){
+   const blankOption = [{ value: '', children: 'choose one'}]
+
+   const aggregateOption = [{
+     value: 'Aggregate',
+     label: 'Aggregate'
+   }]
+
+   const semesterTitles = arr.filter((course) =>
+       course.subject === subject)
+       .map((course) => {
+         return {
+             value: course.semester,
+             children: course.semester
+           }
+         }
+       )
+
+   return blankOption.concat(aggregateOption, semesterTitles)
+  }
+
+   handleSemesterChange(e){
+     const semester = e.target.value
+     // if 'choose one' option was selected
+     if (semester === '') return
+     $.ajax({
+       url: `/api/professors/${this.state.prof}/${this.state.selectedCourse}/${semester}`,
+       type: 'GET'
+     })
+     .done(reviews => {
+       this.setState({
+         selectedSemester: semester,
+         reviews: reviews[0]
+       })
+     });
+   }
+
   render() {
     const {
       selectedCourse,
@@ -111,7 +113,7 @@ export default class Professor extends React.Component {
     } = this.state
 
     return (
-      <Flex  py={2} justify='center' align='center' wrap>
+      <Flex  py={6} justify='center' align='center' wrap>
         <Box sm={12} lg={6}>
           <Panel theme='secondary'>
             <PanelHeader>
